ff89d22ee050ecf02164e3b9fa170338c1fd2801
Игорь Воротников <igor.vorotnikov@yandex.ru>
2021-03-09 11:24:02 +0300

    Add time and space complexity to singly linked list.

--- LinkedList/SinglyLinkedList.h
+++ LinkedList/SinglyLinkedList.h
@@ -3,6 +3,10 @@
 #include <stdexcept>
 #include "SinglyLinkedListNode.h"
 
+//Worst and average time complexity
+//Access - O(n), Search - O(n), Insertion - O(1), Deletion - O(1)
+//Worst space complexity - O(n)
+
 class SinglyLinkedList
 {
 	SinglyLinkedListNode* head;
